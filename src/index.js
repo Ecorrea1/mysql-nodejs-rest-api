9886f8e3b0f64918bce5c97e00d6b3712f05cfa8
Emmanuel Correa <ebcorreac@gmail.com>
2022-08-11 01:27:11 -0400

    agregando cors

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const app = express();
-
+var cors = require('cors')
 // Settings
 app.set('port', process.env.PORT || 3000 );
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Configurar cabeceras y cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 // Routes
 // app.use(require('./routes/employees'));
