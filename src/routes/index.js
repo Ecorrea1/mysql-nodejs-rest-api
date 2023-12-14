578b5b146fd08e2faf6d8758067cfe2af17d35b5
Emmanuel Correa <ebcorreac@gmail.com>
2023-12-14 00:02:31 -0300

    [ADD]Login

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = Router();
 function routerApi(app) {
     app.use('/api', router); 
     router.use('/users', require('./users'));
+    router.use('/auth', require('./auth'));
     router.use('/registers', require('./registers'));
     router.use('/cristals', require('./cristals'));
     router.use('/treatment', require('./treatment'));
---
+++ src/services/auth.services.js
@@ -0,0 +1,27 @@
+const { response } = require('express');
+const { poolConnection }  = require('../config/database.js');
+const { ResultwithData, DataError, ServerError } = require('../helpers/result.js');
+
+const login = async ( req, res = response ) => {
+  try {
+    const { rut, pass } = req.body;
+    const query = `SELECT * FROM users WHERE rut = ${ rut } AND pass = ${ pass }`;
+    poolConnection.query(query, (err, rows, fields) => {
+      if(err) return DataError(res, err);
+      console.log(rows);
+      
+      return ResultwithData(res,'Usuario Correcto', rows);
+    });
+
+    // if(!userExist) return DataError(res, 'Usuario Incorrecto');
+    // return ResultwithData(res,'Usuario Correcto', userExist);
+
+  } catch (err) {
+    console.log(err.message);
+    return ServerError(res, err);
+  }
+}
+
+module.exports = {
+  login
+}
